d9c6bd3e017c5d0c8ba8ee8c52bac28852ab399e
Gilbertirumva <167775136+GilbertIrumva@users.noreply.github.com>
2026-09-02 19:59:04 +0300

    completed exercise 11 Patientor backend, step3

--- patientor/backend/index.ts
+++ patientor/backend/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import diagnosisService from './src/services/diagnosesService.js';
 
 const app = express();
 
@@ -9,6 +10,10 @@ const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
+});
+
+app.get('/api/diagnoses', (_req, res) => {
+  res.json(diagnosisService.getDiagnoses());
 });
 
 app.listen(PORT, () => {
